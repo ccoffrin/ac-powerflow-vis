25c2bcf602c9b1fa46cc099d7b0a16bc1e7a893a
ccoffrin <carleton@coffrin.com>
2017-02-20 13:30:59 -0700

    adding commented code in transformer animation and cleaning up readme

--- transformer_animations.r
+++ transformer_animations.r
@@ -2,12 +2,12 @@ source("ac_powerflow_lib.r")
 
 animation_steps = 120
 
-# steps = seq(0, 2*pi, length.out=animation_steps)
+steps = seq(0, 2*pi, length.out=animation_steps)
 
-# for (i in 1:length(steps)) {
-#     ac_powerflow(sprintf("transformer_angle_flow/%03d.pdf",i), y_a = -pi/2, t_a = 0.0 + steps[i])
-#     ac_powerloss(sprintf("transformer_angle_loss/%03d.pdf",i), y_a = -pi/2, t_a = 0.0 + steps[i])
-# }
+for (i in 1:length(steps)) {
+    ac_powerflow(sprintf("transformer_angle_flow/%03d.pdf",i), y_a = -pi/2, t_a = 0.0 + steps[i])
+    ac_powerloss(sprintf("transformer_angle_loss/%03d.pdf",i), y_a = -pi/2, t_a = 0.0 + steps[i])
+}
 
 
 steps = seq(-0.8, 0.8, length.out=animation_steps/2)
